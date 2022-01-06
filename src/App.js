ae6e2b112f249bc9bdf0d108d7a276560c509438
Jose Sal y Rosas <infojasyrc@gmail.com>
2022-01-06 18:29:05 -0500

    Use UserProvider from new UserContext file

--- src/App.js
+++ src/App.js
@@ -3,10 +3,9 @@ import Main from './components/Main/Main'
 
 import ThemeProvider from './providers/ThemeProvider'
 import ActionsProvider from './providers/ActionsProvider'
-import UserProvider from './providers/UserProvider'
 import Routes from './routes/Routes'
 
-import { LayoutProvider } from './shared/contexts'
+import { LayoutProvider, UserProvider } from './shared/contexts'
 
 class App extends Component {
   render() {
