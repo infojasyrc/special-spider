85326b7f8b686023f31ba32b243ba8005601783a
Jose Sal y Rosas <infojasyrc@gmail.com>
2022-03-03 13:45:55 -0500

    feat: update dependencies (#3)

---
+++ src/components/EventView/stories/EventView.stories.tsx
@@ -0,0 +1,19 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+
+import EventView, { EventViewProps } from '../EventView'
+
+export default {
+  title: 'Container/EventView',
+  component: EventView,
+} as Meta
+
+const Template: Story<EventViewProps> = (args) => <EventView {...args} />
+
+export const Empty = Template.bind({})
+Empty.args = {
+  eventType: '',
+  headquarters: [
+    { id: '1', name: 'Piura' },
+    { id: '2', name: 'Lima' },
+  ],
+}
