e058723f03b93f55890a3ea7274e6b7a22e7f7e8
Jose Sal y Rosas <infojasyrc@gmail.com>
2022-03-13 13:38:38 -0500

    feat: showing add event page with headquarters

--- src/pages/Event/Event.tsx
+++ src/pages/Event/Event.tsx
@@ -1,3 +1,44 @@
+import { useEffect, useState } from 'react'
+
+import EventView from '../../components/EventView/EventView'
+
+import { HeadquarterAPI } from '../../shared/api'
+
+import { Headquarter } from '../../shared/entities'
+
 export default function EventPage(): JSX.Element {
-  return <></>
+  const [allHeadquarters, setAllHeadquarters] = useState<Headquarter[]>([])
+  const [loading, setLoading] = useState(false)
+
+  const apiHeadquarters = HeadquarterAPI()
+
+  const fetchHeadquarters = () => {
+    setLoading(true)
+    apiHeadquarters
+      .getAll()
+      .then((headquarters) => {
+        setAllHeadquarters(headquarters)
+      })
+      .catch((error) => {
+        console.log('Error retrieving all headquarters')
+        console.error(error)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }
+
+  useEffect(() => {
+    fetchHeadquarters()
+    /* eslint-disable */
+  }, [])
+
+  return (
+    <EventView
+      eventType=""
+      headquarters={allHeadquarters}
+      headquarter=""
+      isLoading={loading}
+    />
+  )
 }
