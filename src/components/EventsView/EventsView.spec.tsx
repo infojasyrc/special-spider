8d0189f86558e9edd91828f8577091d89b1dfae3
Jose Sal y Rosas <infojasyrc@gmail.com>
2023-01-19 19:37:41 -0500

    feat: added unit tests focus with data on loaded

--- src/components/EventsView/EventsView.spec.tsx
+++ src/components/EventsView/EventsView.spec.tsx
@@ -1,3 +1,90 @@
+import { render, screen } from '@testing-library/react'
+import { Conference, Headquarter } from '../../shared/entities'
+
+import EventsView, { EventsViewProps } from './EventsView'
+
+const renderComponent = (props: EventsViewProps) =>
+  render(<EventsView {...props} />)
+
+const mockEvent01: Conference = {
+  id: '0001',
+  eventDate: '2023-01-19',
+  name: 'Event 01',
+  status: 'created',
+  year: 2023,
+}
+
+const mockEvent02: Conference = {
+  id: '0002',
+  eventDate: '2023-04-19',
+  name: 'Event 02',
+  status: 'created',
+  year: 2023,
+}
+
+const mockEvent03: Conference = {
+  id: '0003',
+  eventDate: '2023-08-19',
+  name: 'Event 03',
+  status: 'created',
+  year: 2023,
+}
+
 describe('events view component', () => {
-  it('should render all elements', () => {})
-});
+
+  describe('with loaded data', () => {
+    it('should render all elements without data', () => {
+      const mockEvents: Conference[] = []
+      const mockHeadquarters: Headquarter[] = []
+      const props: EventsViewProps = {
+        events: mockEvents,
+        allHeadquarters: mockHeadquarters,
+        loadingEvents: false,
+        loadingHeadquarters: false,
+        isAdmin: false,
+        onSelectedEvent: jest.fn(),
+        changeHeadquarter: jest.fn(),
+        changeFilters: jest.fn(),
+      }
+      renderComponent(props)
+      const eventsTitle = screen.getByText(/events/i)
+      const headquarterTitle = screen.getByLabelText(/headquarter/i)
+  
+      expect(eventsTitle).toBeInTheDocument()
+      expect(headquarterTitle).toBeInTheDocument()
+    })
+
+    it('should render with 3 events', () => {
+      const mockEvents: Conference[] = [
+        mockEvent01,
+        mockEvent02,
+        mockEvent03,
+      ]
+      const mockHeadquarters: Headquarter[] = []
+      const props: EventsViewProps = {
+        events: mockEvents,
+        allHeadquarters: mockHeadquarters,
+        loadingEvents: false,
+        loadingHeadquarters: false,
+        isAdmin: false,
+        onSelectedEvent: jest.fn(),
+        changeHeadquarter: jest.fn(),
+        changeFilters: jest.fn(),
+      }
+      renderComponent(props)
+      const eventsTitle = screen.getByText(/events/i)
+      const headquarterTitle = screen.getByLabelText(/headquarter/i)
+  
+      const eventElement01 = screen.getByText(/event 01/i)
+      const eventElement02 = screen.getByText(/event 02/i)
+      const eventElement03 = screen.getByText(/event 03/i)
+
+      expect(eventsTitle).toBeInTheDocument()
+      expect(headquarterTitle).toBeInTheDocument()
+
+      expect(eventElement01).toBeInTheDocument()
+      expect(eventElement02).toBeInTheDocument()
+      expect(eventElement03).toBeInTheDocument()
+    })
+  });
+})
